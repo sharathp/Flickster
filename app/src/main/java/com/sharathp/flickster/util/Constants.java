4e1d1cb34498bf8a6c606c1c8db97a6633bdab02
sharathp <chandra.ps@gmail.com>
2016-07-20 23:35:32 -0400

    Added item layout, in progress.

--- app/src/main/java/com/sharathp/flickster/util/Constants.java
+++ app/src/main/java/com/sharathp/flickster/util/Constants.java
@@ -1,12 +1,20 @@
 package com.sharathp.flickster.util;
 
 public class Constants {
-    public static final String API_QUERY_PARAM = "api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
-    public static final String URL_BASE = " https://api.themoviedb.org/3";
-    public static final String URL_LATEST_MOVIES = URL_BASE + "/movie/now_playing?" + API_QUERY_PARAM;
+    private static final String API_QUERY_PARAM = "api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
+    private static final String URL_BASE = " https://api.themoviedb.org/3";
+    private static final String URL_LATEST_MOVIES = URL_BASE + "/movie/now_playing?" + API_QUERY_PARAM;
 
-    public static final String getImageUrl(final String relativePath) {
-        return null;
+    private static final String URL_BASE_MOVIE_IMAGE = "https://image.tmdb.org/t/p";
+    private static final String URL_MOVIE_POSTER = URL_BASE_MOVIE_IMAGE + "/w342/%s";
+    private static final String URL_BACKDROP_IMAGE = URL_BASE_MOVIE_IMAGE + "/w780/%s";
+
+    public static final String getPosterImageUrl(final String relativePath) {
+        return String.format(URL_MOVIE_POSTER, relativePath);
+    }
+
+    public static final String getBackdropImageUrl(final String relativePath) {
+        return String.format(URL_BACKDROP_IMAGE, relativePath);
     }
 
     public static final String getLatestMoviesUrl() {
