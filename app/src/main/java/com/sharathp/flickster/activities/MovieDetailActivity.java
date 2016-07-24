788a3f034b252a7d9c0286fe8680f461728f86f3
sharathp <chandra.ps@gmail.com>
2016-07-24 00:04:50 -0400

    Movie details in progress.

--- app/src/main/java/com/sharathp/flickster/activities/MovieDetailActivity.java
+++ app/src/main/java/com/sharathp/flickster/activities/MovieDetailActivity.java
@@ -3,16 +3,27 @@ package com.sharathp.flickster.activities;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.sharathp.flickster.R;
 import com.sharathp.flickster.models.Movie;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MovieDetailActivity extends BaseYoutubeActivity {
     public static final String EXTRA_MOVIE = MovieDetailActivity.class.getSimpleName() + ":MOVIE";
 
     private Movie mMovie;
+
+    @BindView(R.id.tv_movie_title)
+    TextView mTitleTextView;
+
+    @BindView(R.id.tv_movie_desc)
+    TextView mDescTextView;
+
+    @BindView(R.id.tv_votes)
+    TextView mVotesTextView;
 
     public static Intent createIntent(final Context context, final Movie movie) {
         final Intent intent = new Intent(context, MovieDetailActivity.class);
@@ -29,5 +40,8 @@ public class MovieDetailActivity extends BaseYoutubeActivity {
         ButterKnife.bind(this);
 
         mMovie = (Movie) getIntent().getSerializableExtra(EXTRA_MOVIE);
+        mTitleTextView.setText(mMovie.getTitle());
+        mDescTextView.setText(mMovie.getOverview());
+        mVotesTextView.setText(Float.toString(mMovie.getVoteAverage()));
     }
 }
