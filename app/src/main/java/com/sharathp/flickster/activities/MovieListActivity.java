530125b570cc6c085391fa41cad4f78894ee36b1
sharathp <chandra.ps@gmail.com>
2016-07-24 15:55:37 -0400

    Replaced ButterKnife with data binding in movie list activity.

--- app/src/main/java/com/sharathp/flickster/activities/MovieListActivity.java
+++ app/src/main/java/com/sharathp/flickster/activities/MovieListActivity.java
@@ -1,6 +1,7 @@
 package com.sharathp.flickster.activities;
 
 import android.content.Intent;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
@@ -10,6 +11,7 @@ import android.widget.Toast;
 
 import com.sharathp.flickster.FlicksterApplication;
 import com.sharathp.flickster.R;
+import com.sharathp.flickster.databinding.ActivityMovieListBinding;
 import com.sharathp.flickster.models.Movie;
 import com.sharathp.flickster.repositories.MovieRepository;
 import com.sharathp.flickster.views.MovieListAdapter;
@@ -17,33 +19,25 @@ import com.sharathp.flickster.views.MovieListAdapter;
 import java.util.ArrayList;
 import java.util.List;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
-
 public class MovieListActivity extends AppCompatActivity implements MovieRepository.MoviesListCallback, MovieListAdapter.MovieItemCallback {
     private MovieListAdapter mMovieListAdapter;
     private MovieRepository mMovieRepository;
 
-    @BindView(R.id.rv_movies)
-    RecyclerView mMoviesRecyclerView;
-
-    @BindView(R.id.srl_movies)
-    SwipeRefreshLayout mMoviesSwipeContainer;
+    private ActivityMovieListBinding mBinding;
+    private SwipeRefreshLayout mMoviesSwipeContainer;
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_movie_list);
-
-        ButterKnife.bind(this);
-
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_movie_list);
         mMovieListAdapter = new MovieListAdapter(new ArrayList<Movie>(), this);
-        mMoviesRecyclerView.setAdapter(mMovieListAdapter);
-        mMoviesRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-
         mMovieRepository = FlicksterApplication.from(this).getComponent().getMovieRepository();
-        mMovieRepository.retrieveAllMovies(this);
 
+        final RecyclerView moviesRecyclerView = mBinding.rvMovies;
+        moviesRecyclerView.setAdapter(mMovieListAdapter);
+        moviesRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        mMoviesSwipeContainer = mBinding.srlMovies;
         mMoviesSwipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
@@ -52,6 +46,8 @@ public class MovieListActivity extends AppCompatActivity implements MovieReposit
                 mMovieRepository.retrieveAllMovies(MovieListActivity.this);
             }
         });
+
+        mMovieRepository.retrieveAllMovies(this);
     }
 
     @Override
